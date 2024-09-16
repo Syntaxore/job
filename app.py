908910291a944e79b864c516427569bad93d533b
unknown <bogdanfilippov@yahoo.com>
2024-09-16 20:59:25 +0300

    0.3

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask
-from flask import request
-from flask import render_template
+from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', "POST"])
 def home():
-    return render_template("home.html")
+    return render_template("register.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        return f"Вы зарегистрированы! Логин: {username}, Пароль: {password}"
     return render_template("register.html")
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    return render_template("login.html")
 if __name__ == "__main__":
     app.run(debug=True)
